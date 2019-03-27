9e82ec672b0530e0c9fc6936e899065e9fceab27
Nabeel Virani <nabeelvirani@gmail.com>
2019-03-26 19:12:57 -0500

    added search modal and the search input field.

--- src/components/SearchModal/SearchModal.js
+++ src/components/SearchModal/SearchModal.js
@@ -9,35 +9,27 @@ const SearchModal = props => {
   return (
     <div
       id="my-id"
-      class={classNames("uk-modal-full uk-open uk-display-block", { "uk-open": showSearchModal })}
+      class={classNames("uk-modal-full uk-open uk-display-block", {
+        "uk-open": showSearchModal
+      })}
       uk-modal="true"
     >
-      <div class="uk-modal-dialog">
+      <div class="uk-modal-dialog uk-height-1-1 uk-flex uk-flex-middle">
         <button
           class="uk-modal-close-full uk-close-large"
           type="button"
           uk-close="true"
           onClick={toggleSearchModal}
         />
-        <div
-          class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle"
-          uk-grid="true"
-        >
-          <div
-            class="uk-background-cover"
-            uk-height-viewport="true"
-          />
-          <div class="uk-padding-large">
-            <h1>Headline</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+        <div class="uk-flex-1">
+          <div id="search-bar-container">
+            <h3>Search for shit</h3>
+            <input
+              spellcheck="false"
+              placeholder="Search to Listen"
+              tabindex="1"
+              autofocus
+            />
           </div>
         </div>
       </div>
